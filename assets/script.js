222d6e893b88a787d79328291758a8274f0af625
Focrult <focrult@gmail.com>
2022-10-08 21:51:51 +0800

    push failed #3 - Add JS; for loops, if statements, functions

--- assets/script.js
+++ assets/script.js
@@ -1,18 +1,28 @@
-var topics = ['HTML', 'CSS', 'Git', 'JavaScript'];
-
-for (var x = 0; x < topics.length; x++) {
- console.log(topics[x]);
+var topics = ['HTML','CSS','Git','JavaScript'];
+var randomTopic = topics[Math.floor(Math.random() * topics.length)];
+function ListTopics(){
+  for(var x = 0; x < topics.length; x++){
+    console.log(topics[x]);
+  }
 }
-/*
-if (topic === 'HTML') {
-  console.log("Let's study HTML!");
-} else if (topic === 'CSS') {
-  console.log("Let's study CSS!");
-} else if (topic === 'Git') {
-  console.log("Let's study Git!");
-} else if (topic === 'JavaScript') {
-  console.log("Let's study JavaScript!");
-} else {
-  console.log('Please try again!');
+function SelectTopics() {
+  if(randomTopic === 'HTML'){
+    console.log("You have picked HTML");
+  }
+  else if(randomTopic === 'CSS'){
+    console.log("You have picked CSS");
+  }
+  else if(randomTopic === 'Git'){
+    console.log("You have picked Git");
+  }
+  else if(randomTopic === 'JavaScrip'){
+    console.log("You have picked JavaScript");
+  }
+  else{
+    console.log("Error with input");
+  }
 }
-*/
+console.log('Topics that we have worked on throughout Prework:');
+ListTopics();
+console.log('Which topic should we study first?');
+SelectTopics(topics[3]);
